13764a84c2890f91b40193b9f3e11c65750504f8
saransh <sar.bhat0772@yahoo.com>
2016-10-10 00:26:01 +0530

    issues in facebook sign-in ; initialsed register activity

--- app/src/main/java/antaragni/in/antaragni/register.java
+++ app/src/main/java/antaragni/in/antaragni/register.java
@@ -80,17 +80,17 @@ public class register extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.antaragni_register) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.home_register) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.guidelines_register) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.profile_register) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.edit_register) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.logout_register) {
 
         }
 
